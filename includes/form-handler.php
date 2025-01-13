a963b16962a85c2806205f9712ab54895a3c8d8a
Aquilas KIKISSAGBE <kikissagbeaquilas@gmail.com>
2025-01-13 11:23:49 +0100

    Update form submission email handling to notify admin of user submissions

--- includes/form-handler.php
+++ includes/form-handler.php
@@ -468,6 +468,9 @@ add_shortcode('fm_form', 'fm_display_form');
 }*/
 
 
+//Modifier le code suivant pour envoyer seulement un mail qui dit "Un formulaire a été soumis par un utilisateur" à l'administrateur; si l'email de l'administrateur n'est pas renseigné, envoyer le mail à kikissagbeaquilas@gmail.com
+
+
 function fm_handle_submission() {
     if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['fm_submit'])) {
         global $wpdb;
@@ -758,8 +761,6 @@ add_action('admin_menu', 'fm_register_admin_page');*/
 
     echo '</table>';
 }*/
-
-
 
 
 
